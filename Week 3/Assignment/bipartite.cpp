400bdcae39b927a18795caaf159387e60cf1c854
ParthoKR <partho.rajvor2016@gmail.com>
2020-09-22 15:54:19 +0600

    Updated bipartite checking

--- Week 3/Assignment/bipartite.cpp
+++ Week 3/Assignment/bipartite.cpp
@@ -1,42 +1,59 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using std::vector;
-using std::queue;
-
-int bipartite(vector<vector<int> > &adj) {
-  //write your code here
-  vector<int> colour(adj.size(), -1);
-  queue<int> q;
-  q.push(0);
-  colour[0] = 1;
-  while(q.empty()==false){    // Iterating over the queue
-    int current = q.front();
-    q.pop();
-
-    for(int neighbour: adj[current]){   // Iterating over each neighbour
-      if(colour[neighbour] == -1){     // If the node is unvisited
-        colour[neighbour] = 1 - colour[current];  // Assign the opposite colour
-        q.push(neighbour);              // Push in the queue
-      }
-      else if(colour[neighbour] == colour[current]){  // If the colour of neighbour is same as current node return false
-        return 0;
-      }
+#include <bits/stdc++.h>
+using namespace std;
+#define vvi vector<vector<int>>
+#define vi vector<int>
+#define qi queue<int>
+int bfs(vvi &adj, int s, vi &disc, vi &dist)
+{
+    disc[s] = 1; dist[s] = 0;
+    qi q;
+    q.push(s);
+    while(!q.empty())
+    {
+        int u = q.front(); q.pop();
+        for(int i: adj[u])
+        {
+            if(!disc[i])
+            {
+                disc[i] = 1;
+                q.push(i);
+                dist[i] = dist[u]+1;
+            }else if(dist[u]==dist[i])
+            {
+                return 0;
+            }
+        }
     }
-  }
-  return 1;
+    return 1;
 }
+bool isBipartite(vvi &adj, vi &disc, vi &dist)
+{
+    for(int i=0;i<adj.size();++i)
+    {
+        if(!disc[i])
+        {
+            if(!bfs(adj, i, disc, dist))
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vvi adj(n);
+    for(int i=0;i<m;++i)
+    {
+        int x, y;
+        cin >> x >> y;
+        adj[x-1].push_back(y-1);
+        adj[y-1].push_back(x-1);
+    }
+    vi dist(n);
+    vi disc(n, 0);
+    cout << isBipartite(adj, disc, dist);
 
-int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  std::cout << bipartite(adj);
 }
